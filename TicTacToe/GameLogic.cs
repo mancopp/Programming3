ec0c676e04b70eb1b4da7190507a6b8accba9075
mancopp <mancoppor@gmail.com>
2022-10-23 17:19:53 +0200

    Added TicTacToe

---
+++ TicTacToe/GameLogic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class GameLogic
+    {
+        public string CurrentPlayer { get; set; } = X;
+        private const string X = "X";
+        private const string O = "O";
+        private string[,] board = new string[3, 3];
+
+        public void SetNextPlayer()
+        {
+            if(CurrentPlayer == X)
+            {
+                CurrentPlayer = O;
+            }
+            else
+            {
+                CurrentPlayer = X;
+            }
+        }
+
+        public bool PlayerWin()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (!String.IsNullOrWhiteSpace(board[i, 0]))
+                {
+                    if (board[i, 0] == board[i, 1] && board[i, 0] == board[i, 2]) return true;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (!String.IsNullOrWhiteSpace(board[0, i]))
+                {
+                    if (board[0, i] == board[1, i] && board[0, i] == board[2, i]) return true;
+                }
+            }
+
+            if (!String.IsNullOrWhiteSpace(board[1, 1]))
+            {
+                if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2]) return true;
+                if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0]) return true;
+            }
+
+            return false;
+        }
+
+        internal void UpdateBoard(Position pos, string curr)
+        {
+            board[pos.x, pos.y] = curr;
+        }
+    }
+}
---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        GameLogic _GameLogic = new GameLogic();
+
+        private void GameFieldClickHandler(object sender, RoutedEventArgs e)
+        {
+            var field = (Button)sender;
+            if (!String.IsNullOrWhiteSpace(field.Content?.ToString())) return;
+            field.Content = _GameLogic.CurrentPlayer;
+
+            var coordinates = field.Tag.ToString()?.Split(',');
+            var xVal = int.Parse(coordinates[0]);
+            var yVal = int.Parse(coordinates[1]);
+            var buttonPos = new Position() {x = xVal, y = yVal};
+            _GameLogic.UpdateBoard(buttonPos, _GameLogic.CurrentPlayer);
+
+            if (_GameLogic.PlayerWin())
+            {
+                WinScreen.Text = $"Player {_GameLogic.CurrentPlayer} wins!";
+                WinScreen.Visibility = Visibility.Visible;
+            }
+                
+            _GameLogic.SetNextPlayer();
+        }
+
+        private void NewGameBtn_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var el in MainBoard.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Content = String.Empty;
+                }
+            }
+            _GameLogic = new GameLogic();
+            WinScreen.Visibility = Visibility.Collapsed;
+        }
+
+    }
+}
