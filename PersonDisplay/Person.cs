fcfa29b88b7ff952359e2215fbfbd006675c110a
136240 <136240@student.uni.opole.pl>
2022-11-18 10:57:41 +0100

    Added PersonDisplay

---
+++ PersonDisplay/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDisplay
+{
+    public class Person
+    {
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public string pesel;
+
+        public Person()
+        {
+            this.firstName = "Jan";
+            this.lastName = "Kowalski";
+        }
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Person(Person osoba)
+        {
+            this.firstName = osoba.firstName;
+            this.lastName = osoba.lastName;
+        }
+    }
+}
